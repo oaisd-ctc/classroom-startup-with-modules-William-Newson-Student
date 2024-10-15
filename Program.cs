1efe15ddc892783a1db15a6d7f6ef7ebebe14aca
William-Newson-Student <wine2a0@student.oaisd.org>
2024-10-15 17:04:21 +0000

    made a daily classroom startup task list using modules

--- Program.cs
+++ Program.cs
@@ -4,67 +4,49 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        FindSeat();
+        SignIn();
+        DailyLogic();
+        BoxBreathing(4, 5);
+        DailyHypothetical();
+    }
+    public static void FindSeat()
+    {
         Console.WriteLine("Walk in the room.");
         Console.WriteLine("Walk towards desk.");
         Console.WriteLine("Pull out chair.");
         Console.WriteLine("Take off backpack.");
         Console.WriteLine("Place backpack on the ground.");
         Console.WriteLine("Sit down in chair.");
+    }
+    public static void SignIn()
+    {
         Console.WriteLine("Turn on computer.");
         Console.WriteLine("Sign in to computer.");
         Console.WriteLine("Open browser.");
+    }
+    public static void DailyLogic()
+    {
         Console.WriteLine("Navigate to the Daily Logic Classroom.");
         Console.WriteLine("Complete the Daily Logic for the day.");
-        Console.WriteLine("Inhale for 4 seconds.");
-        Console.WriteLine("Hold breath for 4 seconds.");
-        Console.WriteLine("Exhale for 4 seconds.");
-        Console.WriteLine("Hold breath for 4 seconds.");
-        Console.WriteLine("Inhale for 4 seconds.");
-        Console.WriteLine("Hold breath for 4 seconds.");
-        Console.WriteLine("Exhale for 4 seconds.");
-        Console.WriteLine("Hold breath for 4 seconds.");
-        Console.WriteLine("Inhale for 4 seconds.");
-        Console.WriteLine("Hold breath for 4 seconds.");
-        Console.WriteLine("Exhale for 4 seconds.");
-        Console.WriteLine("Hold breath for 4 seconds.");
-        Console.WriteLine("Inhale for 4 seconds.");
-        Console.WriteLine("Hold breath for 4 seconds.");
-        Console.WriteLine("Exhale for 4 seconds.");
-        Console.WriteLine("Hold breath for 4 seconds.");
-        Console.WriteLine("Inhale for 4 seconds.");
-        Console.WriteLine("Hold breath for 4 seconds.");
-        Console.WriteLine("Exhale for 4 seconds.");
-        Console.WriteLine("Hold breath for 4 seconds.");
+    }
+    public static void BoxBreathing(int Time, int Rounds)
+    {
+        for (int i = 0; i < Rounds; i++) 
+        {
+            Console.WriteLine("Inhale for " + Time + " seconds.");
+            Console.WriteLine("Hold breath for " + Time + " seconds.");
+            Console.WriteLine("Exhale for " + Time + " seconds.");
+            Console.WriteLine("Hold breath for " + Time + " seconds.");
+        }
+    }
+    public static void DailyHypothetical()
+    {
         Console.WriteLine("Open a new tab in the browser.");
         Console.WriteLine("Navigate to Google.");
         Console.WriteLine("Open Gmail.");
         Console.WriteLine("Read Daily Hypothetical.");
         Console.WriteLine("Talk to neighbors about Daily Hypothetical.");
         Console.WriteLine("Respond to Daily Hypothetical email.");
-
-        // STEP 3.b - Uncomment the lines below after your functions have been defined
-        // FindSeat();
-        // SignIn();
-        // DailyLogic();
     }
-
-    public static void FindSeat()
-    {
-
-    }
-
-    public static void SignIn()
-    {
-
-    }
-
-    public static void DailyLogic()
-    {
-
-    }
-
-    /**
-    * Create functions for Box Breathing and Daily Hypotheticals below:
-    */
-
 }
